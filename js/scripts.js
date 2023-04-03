c5e911d000e9aa17d2a71595e53eb184768f4b75
ivaflowers <107657852+ivaflowers@users.noreply.github.com>
2023-04-03 16:46:14 +0200

    JS - Version 1.0

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-console.log ('Hello world');
+console.log('Hello, welcome to my website! Hope you will enjoy exploring it!')
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+ } else {
+x.className = 'navtoggle';
+ }
+}
+
